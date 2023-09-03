3c7bd25afb8e10f760616e4785cbfd0d525bb14c
Antonmak112 <pyanton112@gmail.com>
2023-09-03 18:33:36 +0500

    CROS

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +10,12 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user").setViewName("user");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Позволяет CORS для всех URL
+                .allowedOrigins("*") // Разрешает доступ со всех доменов
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Разрешенные HTTP-методы
+                .allowedHeaders("*"); // Разрешенные заголовки
     }
 }
